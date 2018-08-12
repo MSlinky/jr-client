34339e74f85b503c1e421d587ffefc28aedfd3e2
MSlinky <wwwmario15@hotmail.com>
2018-08-11 20:07:16 -0500

    section Quienes somos

---
+++ src/components/generals/quienesSomos.js
@@ -0,0 +1,36 @@
+import React from "react";
+import Grid from '@material-ui/core/Grid';
+
+class QuienesSomos extends React.Component {
+	componentDidMount(prevProps) {
+        
+	}
+  	render() {
+    	return (
+			<div className="quienes-somos">
+                <Grid container>
+                    <Grid item xs={12} md={6} className="title">
+                        <img data-aos="zoom-in" data-aos-offset="350" data-aos-duration="1000" src="/public/img/elementos/titulo_quienes-somos.png" />
+                    </Grid>
+                    <Grid item xs={12} md={6} className="info primary-text">
+                        <div className="dulces-img">
+                            <img data-aos="flip-left" data-aos-offset="350"  data-aos-duration="1000" src="/public/img/elementos/dulces_quienes-somos.png" alt=""/>
+                        </div>
+                        <div className="dulces-text">
+                            <p data-aos="zoom-in" data-aos-offset="350" data-aos-duration="1000">
+                                Junior Producciones, busca hacer de cada fiesta y cumpleaños un recuerdo invaluable; el recuerdo de un
+                                sueño cumplido, el día que conocen a su ídolo, aquel personaje que sólo en fantasías nos visita, hecho realidad
+                                en el día mas especial de los pequeños.
+                            </p>
+                            <div className="hongos">
+                                <img data-aos="flip-left" data-aos-offset="150"  data-aos-duration="1000" src="/public/img/elementos/hongos_quienes-somos.png" alt=""/>
+                            </div>
+                        </div>
+                    </Grid>
+                </Grid>
+			</div>
+    	);
+  	}
+}
+
+export default QuienesSomos;
--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,6 +1,7 @@
 import React from "react";
 import Header from "./../header";
 import HeaderIndex from "./../generals/header-index";
+import QuienesSomos from "./../generals/quienesSomos";
 
 class Home extends React.Component {
 	componentDidMount(prevProps) {
@@ -13,9 +14,7 @@ class Home extends React.Component {
 			<div>
 				<HeaderIndex />
 				<Header />
-				<div style={{height: '500px'}}>
-					
-				</div>
+				<QuienesSomos />
 			</div>
     	);
   	}
